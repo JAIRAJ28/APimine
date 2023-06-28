056bc7effd4e4e93f01501ea303a722927bc903f
JAI RAJ SINGH <jairajsingh28@gmail.com>
2023-06-28 19:14:55 +0530

    CORS

--- node-chatgpt-api.js
+++ node-chatgpt-api.js
@@ -5,7 +5,7 @@ const express = require("express");
 const app=express();
 
 app.use(express.json())
-
+app.use(cors())
   const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
   });
