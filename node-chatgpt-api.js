4d6a4fe287721da127672765c22597fe391b206e
JAI RAJ SINGH <jairajsingh28@gmail.com>
2023-06-29 02:17:37 +0530

    check

--- node-chatgpt-api.js
+++ node-chatgpt-api.js
@@ -1,61 +1,42 @@
-var cors = require('cors')
-const { Configuration, OpenAIApi } = require("openai");
-const readlineSync = require("readline-sync");
-require("dotenv").config();
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+const app = express();
+require("dotenv").config()
 
-const express = require("express");
-const app=express();
 app.use(cors())
+app.use(express.json());
 
-
-app.use(express.json())
-
-
-  const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-  });
-  const openai = new OpenAIApi(configuration);
-
- const history =[]
-app.get("/api/chats",async(req,res)=>{
-    const {user_input}=req.query
-    console.log(user_input)
-
-    const messages = [];
-    for (const [input_text, completion_text] of history) {
-      messages.push({ role: "user", content: input_text });
-      messages.push({ role: "assistant", content: completion_text });
-    }
-    console.log("HELLO")
-    messages.push({ role: "user", content:`Crack a joke on ${user_input}` });
-
-    try {
-      const completion = await openai.createChatCompletion({
-        model: "gpt-3.5-turbo",
-        messages: messages,
-      });
-
-      const completion_text = completion.data.choices[0].message.content;
-      console.log(completion_text);
-      
-      history.push([user_input, completion_text]);
-
-       return(res.status(200).send({completion_text}))
-      
-   
-
-    } catch (error) {
-      if (error.response) {
-        console.log(error.response.status);
-        console.log(error.response.data);
-      } else {
-        console.log(error.message);
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+app.get('/shayari/:word', async (req, res) => {
+  try {
+    const { word } = req.params;
+    console.log(word)
+    // Make a request to the ChatGPT API
+    const response = await axios.post('https://api.openai.com/v1/chat/completions',{
+      model: 'gpt-3.5-turbo', // Update with your desired model
+      messages: [
+        { role: 'system', content: 'You are a poet.' },
+        { role: 'user', content: `Generate a shayari for ${word} in 
+        English` }
+      ],
+      max_tokens: 50,
+      temperature: 0.7
+    }, {
+      headers: {
+        'Authorization': `Bearer ${OPENAI_API_KEY}`, // Replace with your ChatGPT API key
+        'Content-Type': 'application/json'
       }
-    }
-})
+    });
 
-app.listen(process.env.Port,()=>{
-    console.log("connected")
-})
+    const shayari = response.data.choices[0].message.content;
+    res.json({ shayari });
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'An error occurred' });
+  }
+});
 
-
+app.listen(process.env.Port, () => {
+  console.log(`Server is running on port ${process.env.Port}`);
+});
